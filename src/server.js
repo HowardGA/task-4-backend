23330884c3570d9ca018489bb53584498efaf31c
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-05-28 13:10:39 -0700

    Added frontend url for cors

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(session(sessionConfig)); 
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: 'https://task-4-frontend-one.vercel.app/', 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
